5097db2a8cd4c5166dd0879a78b152868224a515
L Sponza <61894715+SponzaPlusPlus@users.noreply.github.com>
2022-06-13 11:18:18 +1000

    Update generalmathcmodule.c

--- C/generalmathcmodule.c
+++ C/generalmathcmodule.c
@@ -1,1 +1,9 @@
+// Call necessary modules/libraries via header files
+
 // ...
+
+float calcYViaLinearFuncFloatV000(float grad, float x_val, float offset)
+{
+	float result = ((grad * x_val) + offset);
+	return result;
+}
