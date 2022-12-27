b56b248ffb0978baeda267585c2067bfd9cee1db
SponzaPlusPlus <61894715+SponzaPlusPlus@users.noreply.github.com>
2022-12-27 20:32:32 +1100

    Update GeneralMathProbJSModule.js
    added variance and standard deviation formulas

--- JavaScript/GeneralMathProbJSModule.js
+++ JavaScript/GeneralMathProbJSModule.js
@@ -1,6 +1,22 @@
 //  GeneralMathProbJSModule.js
 
 //  Coursera Kennesaw State University Six Sigma Green Belt Specialisation (SSGBSpec) Course 02: Adavanced Define and Measure Phases (ADMP)
+//  Week 04, Video 2 - Central Limit Theorem
+//  sigma ^ 2 = sigma_x-bar ^ 2 = sigma_x ^ 2 / n
+function calcVarianceViaSigmaxNV000(sigma_x, n)
+{
+    return (sigma_x ^ 2 / n);
+}
+
+//  Coursera Kennesaw State University Six Sigma Green Belt Specialisation (SSGBSpec) Course 02: Adavanced Define and Measure Phases (ADMP)
+//  Week 04, Video 2 - Central Limit Theorem
+//  Standard Deviation: sigma = sigma_x-bar = sigma_x / sqrt(n)
+function calcStdDevViaSigmaxNV000(sigma_x, n)
+{
+    return (sigma_x / (n ^ (1 / 2)));
+}
+
+//  Coursera Kennesaw State University Six Sigma Green Belt Specialisation (SSGBSpec) Course 02: Adavanced Define and Measure Phases (ADMP)
 //  Week 04, Video 1 - Combinations & Permutations
 function calcPermutationsViaNRV000(n_obj_sets, r_objs)
 {
