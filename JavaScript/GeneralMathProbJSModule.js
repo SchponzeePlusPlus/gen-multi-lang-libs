b7bc5b4fcb907d822e65d49277626958300e5102
SponzaPlusPlus <61894715+SponzaPlusPlus@users.noreply.github.com>
2022-12-27 20:16:13 +1100

    Update GeneralMathProbJSModule.js
    added nCr formula

--- JavaScript/GeneralMathProbJSModule.js
+++ JavaScript/GeneralMathProbJSModule.js
@@ -1,8 +1,17 @@
 //  GeneralMathProbJSModule.js
 
-//  Coursera SSGBSpec_02_ADMP
+//  Coursera Kennesaw State University Six Sigma Green Belt Specialisation (SSGBSpec) Course 02: Adavanced Define and Measure Phases (ADMP)
+//  Week 04, Video 1 - Combinations & Permutations
 function calcPermutationsViaNRV000(n_obj_sets, r_objs)
 {
     // nPr; ((n!) / (n - r)!)
     return (calcNFactorialV000(n_obj_sets) / calcNFactorialV000(n_obj_sets - r_objs));
+}
+
+//  Coursera Kennesaw State University Six Sigma Green Belt Specialisation (SSGBSpec) Course 02: Adavanced Define and Measure Phases (ADMP)
+//  Week 04, Video 1 - Combinations & Permutations
+// nCr; ((n!) / [r! * (n - r)!])
+function calcCombinationsViaNRV000(n_obj_sets, r_objs)
+{
+    return (calcNFactorialV000(n_obj_sets) / (calcNFactorialV000(r_objs) * calcNFactorialV000(n_obj_sets - r_objs)));
 }
