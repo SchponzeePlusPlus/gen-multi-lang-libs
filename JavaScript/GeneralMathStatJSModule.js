b2bb3cfbe597c239d30929599d33f75bcce32193
SponzaPlusPlus <61894715+SponzaPlusPlus@users.noreply.github.com>
2023-01-29 20:31:57 +1100

    Update GeneralMathStatJSModule.js

--- JavaScript/GeneralMathStatJSModule.js
+++ JavaScript/GeneralMathStatJSModule.js
@@ -105,11 +105,54 @@ function calcUCLRBarViaDfourRngeavgV000(ctrl_lim_const_D_three, rnge_avg)
 }
 
 //  Coursera Kennesaw State University Six Sigma Green Belt Specialisation (SSGBSpec) Course 02: Adavanced Define and Measure Phases (ADMP)
-//  Week 07 (Process and Performance Capability & Exploratory Data Analysis), Part 1 - Process and Performance Capability, Video 2 - Designing
-//		Process Performance
+//  Week 07 (Process and Performance Capability & Exploratory Data Analysis), Part 1 - Process and Performance Capability, Video 3 - Analyzing
+//		Process Capability
+//	@brief
+//	σ_circumflex = R_bar / d_2
+//	@param =>
+//	@param =>
+//	@param =>
+//	@return estimated standard dev => σ_circumflex
+function calcEstStdDevViaRngeavgDtwoV000(rnge_avg, ctrl_lim_const_D_two)
+{
+	return (rnge_avg / ctrl_lim_const_D_two);
+}
+
+//  Coursera Kennesaw State University Six Sigma Green Belt Specialisation (SSGBSpec) Course 02: Adavanced Define and Measure Phases (ADMP)
+//  Week 07 (Process and Performance Capability & Exploratory Data Analysis), Part 1 - Process and Performance Capability, Video 3 - Analyzing
+//		Process Capability
+//	@brief
+//	C_pk = Min(Z_U, Z_L) / 3
+//	@param Z_U aka std_devs_between_proc_avg_and_usl : number of standard deviations between the process average and the upper specification
+//		limit => Z_U
+//	@param => Z_L
+//	@param
+//	@return Process Performance "Capability Index" => C_pk
+function calcProcPerfCapbIndexViaZuZlV000(Z_U, Z_L)
+{
+	return (calcMinValBetweenTwoValsV000(Z_U, Z_L) / 3);
+}
+
+//  Coursera Kennesaw State University Six Sigma Green Belt Specialisation (SSGBSpec) Course 02: Adavanced Define and Measure Phases (ADMP)
+//  Week 07 (Process and Performance Capability & Exploratory Data Analysis), Part 1 - Process and Performance Capability, Video 3 - Analyzing
+//		Process Capability
+//	@brief
+//	C_p = Tolerance Range / (6 * σ_circumflex)
+//	@param tolerance range =>
+//	@param estimated standard dev =>
+//	@param =>
+//	@return Capability Index => C_p
+function calcCapbIndexViaZuZlV000(tol_rnge, est_std_dev)
+{
+	return (tol_rnge / (6 * est_std_dev));
+}
+
+//  Coursera Kennesaw State University Six Sigma Green Belt Specialisation (SSGBSpec) Course 02: Adavanced Define and Measure Phases (ADMP)
+//  Week 07 (Process and Performance Capability & Exploratory Data Analysis), Part 1 - Process and Performance Capability, Video 3 - Analyzing
+//		Process Capability
 //	@brief
 //	<math equation>
-//	@param
-//	@param
-//	@param
-//	@return <function title or result>
+//	@param =>
+//	@param =>
+//	@param =>
+//	@return <function title or result> =>
