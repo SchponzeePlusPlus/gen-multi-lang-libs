00b95acd40e3a47ee816a85f279ab810eadbefba
SponzaPlusPlus <61894715+SponzaPlusPlus@users.noreply.github.com>
2023-02-05 12:59:14 +1100

    Update GeneralDateTimeModule.js

--- JavaScript/GeneralDateTimeModule.js
+++ JavaScript/GeneralDateTimeModule.js
@@ -42,7 +42,96 @@ const DAYS_PER_WEEK = 7;
 const DAYS_PER_YEAR = 365.2425;
 const WEEKS_PER_YEAR = (DAYS_PER_YEAR / DAYS_PER_WEEK);
 const MONTHS_PER_YEAR = 12;
+const DAYS_PER_MONTH = (DAYS_PER_YEAR / MONTHS_PER_YEAR);
 const WEEKS_PER_MONTH = (WEEKS_PER_YEAR / MONTHS_PER_YEAR);
 //const ...
 
-function 
+// Define constructors for custom objects (similar to C structs and records)
+
+// Define constructor for the complicated date time object - which has a use
+//	case for budgeting
+function defineDateTimeComplexObjV000(secs, mins, hours, days, weeks, fortnights, months, qtr_annuals, semi_annuals, years, two_years, three_years, four_years, five_years, six_years, seven_years, eight_years, nine_years, decades, two_decades, three_decades, four_decades, five_decades, centuries, one_hundred_and_twenty_five_years, one_hundred_and_fifty_years)
+{
+	this.secs = secs;
+	this.mins = mins;
+	this.hours = hours;
+	this.days = days;
+	this.weeks = weeks;
+	this.fortnights = fortnights;
+	this.months = months;
+	this.qtr_annuals = qtr_annuals;
+	this.semi_annuals = semi_annuals;
+	this.years = years;
+	this.two_years = two_years;
+	this.three_years = three_years;
+	this.four_years = four_years;
+	this.five_years = five_years;
+	this.six_years = six_years;
+	this.seven_years = seven_years;
+	this.eight_years = eight_years;
+	this.nine_years = nine_years;
+	this.decades = decades;
+	this.two_decades = two_decades;
+	this.three_decades = three_decades;
+	this.four_decades = four_decades;
+	this.five_decades = five_decades;
+	this.centuries = centuries;
+	this.one_hundred_and_twenty_five_years = one_hundred_and_twenty_five_years;
+	this.one_hundred_and_fifty_years = one_hundred_and_fifty_years;
+}
+
+function defineDateTimeSimplerObjV000(secs, mins, hours, days, weeks, months, years, decades, centuries)
+{
+	this.secs = secs;
+	this.mins = mins;
+	this.hours = hours;
+	this.days = days;
+	this.weeks = weeks;
+	this.months = months;
+	this.years = years;
+	this.decades = decades;
+	this.centuries = centuries;
+}
+
+//	DateTimeSimpleObjV000
+function defineDateTimeSimpleObjV000(secs, mins, hours, days, months, years)
+{
+	this.secs = secs;
+	this.mins = mins;
+	this.hours = hours;
+	this.days = days;
+	this.months = months;
+	this.years = years;
+}
+
+//	@return Days Elapsed
+function convertWeeksElapsedToDaysElapsedV000(weeks_elapsed)
+{
+	return (weeks_elapsed * DAYS_PER_WEEK);
+}
+
+//	@return Days Elapsed
+function convertMonthsElapsedToDaysElapsedV000(months_elapsed)
+{
+	return (months_elapsed * DAYS_PER_MONTH);
+}
+
+//	@return Weeks Elapsed
+function convertDaysElapsedToWeeksElapsedV000(days_elapsed)
+{
+	return (days_elapsed / DAYS_PER_WEEK);
+}
+
+//	@param DateTimeSimpleObjV000 type
+function calcMinsElapsedViaDateTimeSimpleObjV000(date_time_elapsed_simple_obj)
+{
+	let result = 0;
+
+	result = (date_time_elapsed_simple_obj.mins + (60 *
+			(date_time_elapsed_simple_obj.hours + (24 *
+			(date_time_elapsed_simple_obj.days + ((365 / 12) *
+			(date_time_elapsed_simple_obj.months + (12 *
+			(date_time_elapsed_simple_obj.years)))))))));
+
+	return result;
+}
