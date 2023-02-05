783da069ec68f1320061327ae600fe921c30587b
SponzaPlusPlus <61894715+SponzaPlusPlus@users.noreply.github.com>
2023-02-05 14:46:17 +1100

    Update GeneralFinJSModule.js

--- JavaScript/GeneralFinJSModule.js
+++ JavaScript/GeneralFinJSModule.js
@@ -16,7 +16,7 @@
 //	@brief
 //	FV = PV * (1 + i) ^ t
 //	@param =>
-//	@param rate_interest: Interest rate of same time period to period parameter => i
+//	@param rate_interest: Interest rate of same time period to period parameter; Unit of Measurement is a Factor to 1 (0.01 interest rate factor = 1 % interest rate) => i
 //	@param periods_num: number of periods; unspecified time intervals that have elapsed; period intervals must match interest rate time period parameter => t
 //	@return Future Value of Lump Sum => FV
 function calcFutureValLumpSumCompoundIRViaPresvallsIntPerV000(pres_val_lump_sum, rate_interest, periods_num)
@@ -29,7 +29,7 @@ function calcFutureValLumpSumCompoundIRViaPresvallsIntPerV000(pres_val_lump_sum,
 //	@brief
 //	PV = FV / ((1 + i) ^ t)
 //	@param =>
-//	@param rate_interest: Interest rate of same time period to period parameter => i
+//	@param rate_interest: Interest rate of same time period to period parameter; Unit of Measurement is a Factor to 1 (0.01 interest rate factor = 1 % interest rate) => i
 //	@param periods_num: number of periods; unspecified time intervals that have elapsed; period intervals must match interest rate time period parameter => t
 //	@return Present Value of Lump Sum => PV
 function calcPresentValLumpSumCompoundIRViaFuturevallsIntPerV000(future_val_lump_sum, rate_interest, periods_num)
@@ -44,7 +44,7 @@ function calcPresentValLumpSumCompoundIRViaFuturevallsIntPerV000(future_val_lump
 //	@param =>
 //	@param Present Value of Lump Sum => PV
 //	@param periods_num: number of periods; unspecified time intervals that have elapsed; period intervals must match interest rate time period parameter => t
-//	@return rate_interest: Interest rate of same time period to period parameter => i
+//	@return rate_interest: Interest rate of same time period to period parameter; Unit of Measurement is a Factor to 1 (0.01 interest rate factor = 1 % interest rate) => i
 function calcIntRateCompoundedViaFuturevallsPresvallsPerV000(future_val_lump_sum, pres_val_lump_sum, periods_num)
 {
 	return (((future_val_lump_sum / pres_val_lump_sum) ** (1.0 / periods_num)) - 1.0);
@@ -56,11 +56,25 @@ function calcIntRateCompoundedViaFuturevallsPresvallsPerV000(future_val_lump_sum
 //	t = (log(FV / PV)) / (log(1 + i))
 //	@param =>
 //	@param Present Value of Lump Sum => PV
-//	@param rate_interest: Interest rate of same time period to period parameter => i
+//	@param rate_interest: Interest rate of same time period to period parameter; Unit of Measurement is a Factor to 1 (0.01 interest rate factor = 1 % interest rate) => i
 //	@return periods_num: number of periods; unspecified time intervals that have elapsed; period intervals must match interest rate time period parameter => t
 function calcPeriodsNumCompoundedViaFuturevallsPresvallsIntV000(future_val_lump_sum, pres_val_lump_sum, rate_interest)
 {
 	return ((calcLogViaBaseV000(10, (future_val_lump_sum / pres_val_lump_sum))) / (calcLogViaBaseV000(10, (1.0 + rate_interest))));
+}
+
+//  Swinburne University of Technology: Bachelor of Business (Finance) [BB-EngBus]: Financial Management Unit of Study (FIN20014)
+//  Week 2: Topic 2A - Time Value of Money
+//	@brief
+//	i (output period) = i / m
+//	@param rate_interest_nominal: The Nominal Interest Rate (NIR) is the interest rate expressed in terms of the interest payment made each period.;
+//	Interest rate of same time period to Number of compounding periods in the specified output period interval measurement parameter; Unit of Measurement is a Factor to 1
+//	(0.01 interest rate factor = 1 % interest rate) => i
+//	@param compounding_freq_per_output_period: Number of compounding periods per the specified output period interval measurement => m
+//	@return Interest rate of same time period to specified output period interval measurement parameter; Unit of Measurement is a Factor to 1 (0.01 interest rate factor = 1 % interest rate) => i
+function calcIntRateOutFreqViaIntCompperiodsV000(rate_interest_nominal, compounding_freq_per_output_period)
+{
+	return (rate_interest_nominal / compounding_freq_per_output_period);
 }
 
 //	@param in_np Net Profit; financial gain
