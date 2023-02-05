31cea768a67cba4862792f93a8f69ed0ca72f202
SponzaPlusPlus <61894715+SponzaPlusPlus@users.noreply.github.com>
2023-02-05 11:26:13 +1100

    C / C++ / Arduino folder restructuring

---
+++ C/Arduino/GeneralCArduinoModule.c
@@ -0,0 +1,19 @@
+//	GeneralCArduinoModule.c
+
+/***************************************************************************************
+*    Title: <title of program/source code>
+*    Author: <author(s) names>
+*    Date: <date>
+*    Code version: <code version>
+*    Availability: <where it's located>
+*
+***************************************************************************************/
+
+/***************************************************************************************
+*    Title: <title of program/source code>
+*    Author: <author(s) names>
+*    Date: <date>
+*    Code version: <code version>
+*    Availability: <where it's located>
+*
+***************************************************************************************/
