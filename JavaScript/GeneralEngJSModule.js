d27a270cc049bce68bdb60f784186c979611083d
SponzaPlusPlus <61894715+SponzaPlusPlus@users.noreply.github.com>
2024-02-04 17:58:22 +1100

    Update GeneralEngJSModule.js
    Intro to Power Elec ; useless update right now

--- JavaScript/GeneralEngJSModule.js
+++ JavaScript/GeneralEngJSModule.js
@@ -3,6 +3,18 @@ function calcForceViaMAV000(mass, accel)
 	return (mass * accel);
 }
 
+//  Coursera: University of Colorado Boulder: Power Electronics Specialization: Introduction to Power Electronics Course
+//  Week 2: Steady State Conversion Analysis
+//Sect. 2.2 Volt-Sec Balance and the Small Ripple Approximation
+
+//	@brief
+//	i (output period) = i / m
+//	@param rate_interest_nominal: The Nominal Interest Rate (NIR) is the interest rate expressed in terms of the interest payment made each period.;
+//	Interest rate of same time period to Number of compounding periods in the specified output period interval measurement parameter; Unit of Measurement is a Factor to 1
+//	(0.01 interest rate factor = 1 % interest rate) => i
+//	@param compounding_freq_per_output_period: Number of compounding periods per the specified output period interval measurement => m
+//	@return Interest rate of same time period to specified output period interval measurement parameter; Unit of Measurement is a Factor to 1 (0.01 interest rate factor = 1 % interest rate) => i
+
 // Automotive Eng-Finance Calcs
 
 //	@brief Calculation for Fuel Consumed per Period specified
