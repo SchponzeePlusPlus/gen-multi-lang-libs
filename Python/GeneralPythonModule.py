83ccdc830eb661b68b34ded7133deae8886c0f33
SchponzeePlusPlus <61894715+SchponzeePlusPlus@users.noreply.github.com>
2024-05-20 13:34:36 +1000

    Update GeneralPythonModule.py WIP
    quickly added power calcs for electrical, rotational, and hydraulic variables

--- Python/GeneralPythonModule.py
+++ Python/GeneralPythonModule.py
@@ -1,1 +1,18 @@
+/*
 test
+*/
+
+# test in-line
+
+#	Electrical Power
+def calcPwrWViaVoltVCurrA(voltV, currA):
+	return (voltV * currA)
+
+#	Rotational Power
+def calcPwrWViaTrqNmAngVelradps(trqNm, angVelRadPerSec):
+	return (trqNm * angVelRadPerSec)
+
+#	Mecahnical / Hydraulic Power
+#	not sure what the units should be used to calculate Hydraulic Power in Watts
+def calcPwrWViaPressXFloRteY(pressX, floRteY):
+	return (pressX * floRteY)
